e6632dc6f071d9986d0e2a6d37fb7d14e821de17
William Gocheco <isaerwilliamvicsongocheco@gmail.com>
2022-09-16 22:15:54 +0800

    completed calc layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import BasicButton from './components/BasicButton';
 
+const Container = styled.div`
+    background-color: var(--background-color);
+    width: 240px;
+    padding: 5px;
+`
 
-function App() {
+const Display = styled.div`
+  width: 100%;
+  margin-top: 50px;
+  font-size: 50px;
+  color: white;
+  text-align: end;
+`
+
+const App = () => {
+  const [state, setState] = useState('0')
   return (
-    <div className="App">
-      <BasicButton label='1' color='white'/>
-    </div>
+    <Container>
+      <Display>{state}</Display>
+      <div>
+        <BasicButton display={setState} label={state === '0' ? 'AC' : 'C'} color='light' textColor='black' onClick={() => setState('0')} />
+        <BasicButton display={setState} label='+/-' color='light' textColor='black' />
+        <BasicButton display={setState} label='%' color='light' textColor='black' />
+        <BasicButton display={setState} label='÷' color='highlight' />
+
+        <BasicButton display={setState} label='7' color='black' />
+        <BasicButton display={setState} label='8' color='black' />
+        <BasicButton display={setState} label='9' color='black' />
+        <BasicButton display={setState} label='×' color='highlight' />
+
+        <BasicButton display={setState} label='4' color='black' />
+        <BasicButton display={setState} label='5' color='black' />
+        <BasicButton display={setState} label='6' color='black' />
+        <BasicButton display={setState} label='−' color='highlight' />
+
+        <BasicButton display={setState} label='1' color='black' />
+        <BasicButton display={setState} label='2' color='black' />
+        <BasicButton display={setState} label='3' color='black' />
+        <BasicButton display={setState} label='+' color='highlight' />
+
+        <BasicButton display={setState} label='0' color='black' length='medium' />
+        <BasicButton display={setState} label='.' color='black' />
+        <BasicButton display={setState} label='=' color='highlight' />
+      </div>
+    </Container>
   )
 }
 
--- src/components/BasicButton.tsx
+++ src/components/BasicButton.tsx
@@ -1,29 +1,82 @@
-import React from "react"
+import React, { Dispatch, SetStateAction } from "react"
 import styled from "styled-components"
 
 interface Props {
     label: string
     color: string
+    length?: string
+    textColor?: string
+    onClick?: () => void
+    display: Dispatch<SetStateAction<string>>
+}
+interface styledProps {
+    color: string
+    length?: string
+    textColor?: string
 }
 
-const Button = styled.button`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-    width: var(--button-size);
+const Button = styled.button<styledProps>`
+    all: unset;
+    margin: 5px;
+    font-size: 20px;
+    text-align: ${props => props.length === 'medium' ? 'left' : 'center'};
+    padding-left: ${props => props.length === 'medium' && '20px'} ;
     height: var(--button-size);
-    border-radius: 50%;
-    background-color: ${props => {
-        switch (props.color) {
+    border-radius: var(--button-size);
+    cursor: pointer;
+    color: ${props => {
+        switch (props.textColor) {
+            case 'white':
+                return 'white'
             case 'black':
                 return 'black'
+            default:
+                return 'white'
+        }
+    }};
+    width: ${props => {
+        switch (props.length) {
+            case 'small':
+                return 'var(--button-size)'
+            case 'medium':
+                return '90px'
+            default:
+                return 'var(--button-size)'
+        }
+    }};
+    border-color: ${props => {
+        switch (props.color) {
+            case 'dark':
+                return 'var(--dark-color)'
+            case 'light':
+                return 'var(--light-color)'
+            case 'highlight':
+                return 'var(--highlight-color)'
+            default:
+                return 'var(--dark-color)'
+        }
+    }};
+    background-color: ${props => {
+        switch (props.color) {
+            case 'dark':
+                return 'var(--dark-color)'
+            case 'light':
+                return 'var(--light-color)'
+            case 'highlight':
+                return 'var(--highlight-color)'
+            default:
+                return 'var(--dark-color)'
         }
     }};
 `
 
-const BasicButton: React.FC<Props> = ({ label, color }) => {
+const BasicButton: React.FC<Props> = ({ label, color, length, textColor, onClick, display }) => {
+    const handleClick = () => {
+        onClick && onClick();
+        (label === 'AC' || label === 'C') || display((state) => (state === '0' || state === 'C') ? label : state.concat(label))
+    }
     return (
-        <Button color={color}>
+        <Button color={color} length={length} textColor={textColor} onClick={handleClick}>
             {label}
         </Button>
     )
